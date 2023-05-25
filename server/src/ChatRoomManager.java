6b9153a4bdc15f7fbbd01b2f25e25f069817231f
Mehrnaz Sahebi <mehrnazsa83@gmail.com>
2023-05-25 22:20:51 +0330

    feat : constructor and field for class ChatRoomManager

--- server/src/ChatRoomManager.java
+++ server/src/ChatRoomManager.java
@@ -1,2 +1,10 @@
+import java.net.Socket;
+import java.util.HashSet;
+
 public class ChatRoomManager extends Thread{
+    private static HashSet<User> users = new HashSet<User>();
+    private final Socket currentSocket;
+    public ChatRoomManager(Socket client){
+        currentSocket = client;
+    }
 }
