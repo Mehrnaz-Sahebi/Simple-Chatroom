4da0db3d9fa43cc1f163590d8bc208468792bd88
Mehrnaz Sahebi <mehrnazsa83@gmail.com>
2023-05-25 22:25:08 +0330

    feat : fields,constructor,getters and setters for class User

--- common/src/User.java
+++ common/src/User.java
@@ -1,4 +1,49 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.net.Socket;
 
 public class User implements Serializable {
+    private final Socket socket;
+    private String username;
+    private ObjectInputStream inputStream;
+    private ObjectOutputStream outputStream;
+    public User(Socket socket){
+        this.socket = socket;
+        try {
+            outputStream = new ObjectOutputStream(socket.getOutputStream());
+            inputStream = new ObjectInputStream(socket.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public ObjectInputStream getInputStream() {
+        return inputStream;
+    }
+
+    public void setInputStream(ObjectInputStream inputStream) {
+        this.inputStream = inputStream;
+    }
+
+    public ObjectOutputStream getOutputStream() {
+        return outputStream;
+    }
+
+    public void setOutputStream(ObjectOutputStream outputStream) {
+        this.outputStream = outputStream;
+    }
 }
