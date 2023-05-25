d67019c416ce231658e907d64b8994371cce0ef1
Mehrnaz Sahebi <mehrnazsa83@gmail.com>
2023-05-25 22:28:17 +0330

    feat : code in ChatRoomManagers run method to handle new enters

--- server/src/ChatRoomManager.java
+++ server/src/ChatRoomManager.java
@@ -1,12 +1,34 @@
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.HashSet;
 
-public class ChatRoomManager extends Thread{
+public class ChatRoomManager extends Thread {
     private static HashSet<User> users = new HashSet<User>();
     private final Socket currentSocket;
-    public ChatRoomManager(Socket client){
+
+    public ChatRoomManager(Socket client) {
         currentSocket = client;
     }
+
     @Override
-    public void run(){}
+    public void run() {
+        try {
+            User currentUser = new User(currentSocket);
+            String username = (String) currentUser.getInputStream().readObject();
+            currentUser.setUsername(username);
+            users.add(currentUser);
+            System.out.println("User " + username + " entered the chatroom");
+            for (User loopUser : users) {
+                if (!loopUser.getUsername().equals(username)) {
+                    ObjectOutputStream outputStreamToOther = loopUser.getOutputStream();
+                    outputStreamToOther.writeObject("newEnter");
+                    outputStreamToOther.writeObject(username);
+                    System.out.println("sent the notification of " + username + " enter to " + loopUser.getUsername());
+                }
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
