dc47c30813f2d21ee2ac96e53acd4b075a12941d
Mehrnaz Sahebi <mehrnazsa83@gmail.com>
2023-05-25 22:19:46 +0330

    feat : method main in class Server

--- server/src/Server.java
+++ server/src/Server.java
@@ -1,2 +1,19 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(2244);
+            while (true) {
+                Socket client = serverSocket.accept();
+                Thread chatRoomManager = new ChatRoomManager(client);
+                chatRoomManager.start();
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
